3009373aee88c4224beb0020d1247f348e87f6e1
Allenn George K <you@example.com>
2025-01-16 02:28:22 +0530

    eihv

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/allenngkportfolio",  // Ensure the base path is set correctly
+  base: "/portfolio",  // Ensure the base path is set correctly
   build: {
     outDir: 'dist',  // Set the output directory to 'build'
   },
