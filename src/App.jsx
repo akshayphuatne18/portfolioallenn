5d5f6f740a4b70f53393766b142a46a16e2d1b52
Allenn George K <you@example.com>
2025-01-27 02:30:56 +0530

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import About from './components/About';
 import Education from './components/Education';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Certifications from './components/Certifications';
 import Achievements from './components/Achievements';
+import Certifications from './components/Certifications';
 import Footer from './components/Footer';
 
 function App() {
